0d83828ee1ea04491d3e4e971ad196cb0c40e7e5
FreeFlyXiaoMa <2429825530@qq.com>
2019-02-21 13:15:31 +0800

    Initial commit

---
+++ DataPreProcess.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+from matplotlib import pyplot as plt
+
+#读取数据
+train=pd.read
+
+
