1ebde8f2daf38fd55d8699f775740cae98f11b28
FreeFlyXiaoMa <2429825530@qq.com>
2019-02-26 12:01:17 +0800

    散点图

---
+++ scatter.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+y_train=np.array([368,340,376,954,331,556])
+X_train=np.array([1.7,1.5,1.3,5,1.3,2.2])
+plt.scatter(X_train,y_train,label='scatter')
+plt.show()
