622aefdc558d7c9ade5e6d1dacb9522ba1d10708
FreeFlyXiaoMa <2429825530@qq.com>
2019-02-27 09:16:43 +0800

    散点图

--- scatter.py
+++ scatter.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from sklearn.linear_model import LinearRegression
+lr=LinearRegression()
 y_train=np.array([368,340,376,954,331,556])
+y_train=np.reshape(y_train,(-1,1))
 X_train=np.array([1.7,1.5,1.3,5,1.3,2.2])
-plt.scatter(X_train,y_train,label='scatter')
-plt.show()
+X_train=np.reshape(X_train,(-1,1))
+lr.fit(X_train,y_train)
+print('截距项',lr.intercept_)
+print('权重：',lr.coef_[0])
+#plt.scatter(X_train,y_train,label='scatter')
+#plt.show()
