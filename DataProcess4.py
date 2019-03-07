b8d1c7820dcaae595f08afe207a3742b4e9a23b3
FreeFlyXiaoMa <2429825530@qq.com>
2019-03-07 15:17:36 +0800

    Keans聚类数据处理

--- DataProcess4.py
+++ DataProcess4.py
@@ -40,7 +40,7 @@ def K_cluster_analysis(K,X):
 
 #设置超参数搜索范围
 #Ks=[10,20,30,40,50]
-Ks=[5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+Ks=[8,9,10,11,12]
 #Ks=range(2,20)
 CH_scores=[]
 for K in Ks:
@@ -60,9 +60,8 @@ print('Best K is:',Best_k)
 #用最佳的K再次聚类，得到聚类结果
 mb_kmeans=MiniBatchKMeans(n_clusters=Best_k)
 y_pred=mb_kmeans.fit_predict(X_train)
-for i in range(len(y_pred)):
-    print(y_pred[i])
-#print(y_pred)
+
+print(y_pred)
 
 #保存聚类结果
 feat_names_Kmeans='Kmeans_'+str(Best_k)
@@ -70,9 +69,24 @@ y=pd.Series(data=y_train,name='target')
 train_kmeans=pd.concat([train_id,pd.Series(name=feat_names_Kmeans,data=y_pred),y],axis=1)
 train_kmeans.to_csv('Otto_FE_KMeans.csv',index=False,header=True)
 
+#保存模型
+import pickle
+pickle.dump(mb_kmeans,open('mb_kmeans.pkl','wb'))
 
+'''
+#测试数据
+test=pd.read_csv('Otto_test.csv')
+X_test=test.drop(['id'],axis=1)
+print(X_test.shape)
+test_id=test['id']
+normalize(X_test,norm='l2',copy=False)
 
+#加载模型
+mb_kmeans=pickle.load(open('mb_kmeans.pkl','rb'))
+y_test_pred=mb_kmeans.predict(X_test)
 
-
-
+#保存测试结果
+feat_names_Kmeans='Kmean_'+str(mb_kmeans.cluster_centers_)
+test_kmeans=pd.concat([test_id,pd.Series(name=feat_names_Kmeans,data=y_test_pred)],axis=0)
+test_kmeans.to_csv('Otto_test_KMeans.csv')'''
 
